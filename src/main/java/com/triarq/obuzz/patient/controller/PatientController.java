dfa9e85edeeed3355f5757851dc6e86c2c26e4b4
bharat.bhamare <b3.aug95@gmail.com>
2020-02-07 11:30:32 +0530

    role base api

--- src/main/java/com/triarq/obuzz/patient/controller/PatientController.java
+++ src/main/java/com/triarq/obuzz/patient/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.triarq.obuzz.patient.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,28 @@ public class PatientController {
 	@Autowired
 	PatientRepository repo;
 
-	
-	@GetMapping("/patient")
-	public List<Patients> getpatient()
-	{
+	@GetMapping("/patient" )	
+	@PreAuthorize("hasAuthority('read_profile')")
+	public List<Patients> getpatient() {
 		return (List<Patients>) repo.findAll();
+	}
+
+	@GetMapping("/patient/save")
+	@PreAuthorize("hasAuthority('create_profile')")
+	public String savePatient() {
+		return "save patient";
+	}
+
+	@GetMapping("/patient/edit")
+	@PreAuthorize("hasAuthority('update_profile')")
+	public String  editPatient() {
+		return "edit patient";
+	}
+	
+	
+	@GetMapping("/patient/delete")
+	@PreAuthorize("hasAuthority('delete_profile')")
+	public String deletePatient() {
+		return "delete patent";
 	}
 }
